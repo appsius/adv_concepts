3723bc777cdecae724b26bf3e8bcadf24d17d684
appsius <ilyas.ustunkaya234434@gmail.com>
2020-05-27 12:36:04 +0300

    Closing Modal

--- web_comps/web_comp_two/modal.js
+++ web_comps/web_comp_two/modal.js
@@ -69,8 +69,8 @@ class Modal extends HTMLElement {
                <slot></slot>
             </section>
             <section id="actions">
-               <button>Cancel</button>
-               <button>Okay</button>
+               <button id="cancel-btn">Cancel</button>
+               <button id="confirm-btn">Okay</button>
             </section>
 		   </div>
       `;
@@ -78,6 +78,11 @@ class Modal extends HTMLElement {
 		slots[1].addEventListener('slotchange', (event) => {
 			console.dir(slots[1].assignedNodes());
 		});
+
+		const cancelBtn = this.shadowRoot.querySelector('#cancel-btn');
+		const confirmBtn = this.shadowRoot.querySelector('#confirm-btn');
+		cancelBtn.addEventListener('click', this._cancel.bind(this));
+		confirmBtn.addEventListener('click', this._confirm.bind(this));
 	}
 
 	attributeChangedCallback(name, oldValue, newValue) {
@@ -102,6 +107,21 @@ class Modal extends HTMLElement {
 	open() {
 		this.setAttribute('opened', '');
 		this.isOpen = true;
+	}
+
+	hide() {
+		if (this.hasAttribute('opened')) {
+			this.removeAttribute('opened');
+		}
+		this.isOpen = false;
+	}
+
+	_cancel() {
+		this.hide();
+	}
+
+	_confirm() {
+		this.hide();
 	}
 }
 
