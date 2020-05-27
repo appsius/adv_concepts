2d48c21bba8372e45bf6c982264e6148c2e8a6a5
appsius <ilyas.ustunkaya234434@gmail.com>
2020-05-27 06:36:39 +0300

    Observing Attribute Changes

--- web_comps/tooltip.js
+++ web_comps/tooltip.js
@@ -56,6 +56,14 @@ class Tooltip extends HTMLElement {
 		this.shadowRoot.appendChild(tooltipIcon);
 	}
 
+	attributeChangedCallback(name, oldValue, newValue) {
+		console.log(name, oldValue, newValue);
+	}
+
+	static get observedAttributes() {
+		return ['text'];
+	}
+
 	_showTooltip() {
 		this._tooltipContainer = document.createElement('div');
 		this._tooltipContainer.textContent = this._tooltipText;
